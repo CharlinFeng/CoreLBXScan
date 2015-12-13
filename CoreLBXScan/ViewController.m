ae79c4f5500e61383e65c362e6ddb0de9a0b0c8a
时点软件 冯成林 <Charlin@fengchenglindeiMac.local>
2015-12-13 18:40:10 +0800

    init

---
+++ CoreLBXScan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreLBXScan
+//
+//  Created by 冯成林 on 15/12/13.
+//  Copyright © 2015年 冯成林. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
